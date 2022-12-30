fe15cb3adc5b169bd792be2d5725d5e6d1f72902
FilipeRichterSena <filipe.richter.sena@gmail.com>
2022-12-30 09:50:09 -0300

    Ajustando Menu

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace Cronometro
     {
         static void Main(string[] args)
         {
-            Menu();;
+            Menu();
         }
 
         static void Menu()
@@ -17,6 +17,12 @@ namespace Cronometro
             Console.WriteLine("M = Minuto");
             Console.WriteLine("0 = Sair");
             Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0,data.Length - 1));
+            Console.WriteLine(time);
+            Console.WriteLine(type);
         }
 
         static void Start(int time)
